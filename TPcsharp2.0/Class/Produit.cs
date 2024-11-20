5111e3c63ef1b2a74038aba70dd4a47011da8c92
nexus806 <antonin.merlomeyffren@ynov.com>
2024-11-20 18:05:06 +0100

    first commit

---
+++ TPcsharp2.0/Class/Produit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPcsharp2._0
+{
+    internal class Produit
+    {
+        public string Nom;
+        public float Prix;
+        public int QantiteEnStock;
+        public int CodeProduit;
+
+        public Produit(string nom, float prix, int qantiteEnStock, int codeProduit)
+        {
+            Nom = nom;
+            Prix = prix;
+            QantiteEnStock = qantiteEnStock;
+            CodeProduit = codeProduit;
+        }
+
+        public override string ToString()
+        {
+            return $"Produit : {Nom}, Prix : {Prix}, Quantité : {QantiteEnStock}, Code Produit : {CodeProduit}";
+        }
+
+        public void creationProduit()
+        {
+            string newName;
+            float newPrix;
+            int newQantiteEnStock;
+            int newCodeProduit;
+
+            Console.Write("entrez le nom de votre produit : ");
+            newName = Console.ReadLine();
+
+            Console.Write("entrez le prix du produit : ");
+            newPrix = Console.Read();
+
+            Console.Write("entrez le nombre de de produit que vous possédez : ");
+            newQantiteEnStock = Console.Read();
+
+            Console.Write("choisissez le code produit : ");
+            newCodeProduit = Console.Read();
+
+            Console.WriteLine("merci d'avoir enregistrer le produit");
+
+        }
+    }
+}
---
+++ TPcsharp2.0/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPcsharp2._0;
+
+namespace ConsoleAppTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Panier panier = new Panier();
+            int ajoutPanier = 0;
+            
+            List<Produit> produits = new List<Produit>
+            {
+                new Produit("produit 1", 12, 4, 1),
+                new Produit("produit 2", 20, 4, 2),
+                new Produit("produit 3", 50, 4, 3),
+            };
+
+
+            while (true)
+            {
+                Console.Clear(); 
+
+
+
+                Console.WriteLine("   #####    #####   ### ###   #####    ######           ######   #######  ### ###  #######  ##  ###   ######  #######\r\n  ### ###  ### ###  ### ###  ### ###   # ## #           ### ###  ### ###  ### ###  ### ###  ### ###   # ## #  ### ###\r\n  ### ###  ###      ### ###  ### ###     ##             ### ###  ###      ### ###  ###      #######     ##    ###\r\n  #######  ###      #######  #######     ##             ######   #####    ### ###  #####    #######     ##    #####\r\n  ### ###  ###      ### ###  ### ###     ##             ### ##   ###      ### ###  ###      ### ###     ##    ###\r\n  ### ###  ### ###  ### ###  ### ###     ##             ### ###  ### ###   #####   ### ###  ### ###     ##    ### ###\r\n  ### ###   #####   ### ###  ### ###     ##             ### ###  #######    ###    #######  ### ###     ##    #######\r\n\r\n");
+
+                foreach (Produit produit in produits)
+                {
+                    Console.WriteLine(produit);                   
+                }
+
+                if (panier != null)
+                {
+                    panier.afficheContenu();
+                }
+
+                Console.Write("entrez le code produit pour l'ajouter au panier ou entrez 999 si vous voulez ajouter un produit : ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out ajoutPanier))
+                {
+                    Produit produitSelectionner = produits.Find(p => p.CodeProduit == ajoutPanier);
+
+                    if (produitSelectionner.QantiteEnStock > 0)
+                    {
+                        panier.addProduit(produitSelectionner);
+                        produitSelectionner.QantiteEnStock--;
+                        panier.totalProduit(produitSelectionner.Prix);
+                    }
+                    else
+                    {
+                        Console.WriteLine("cette article à été victime de son succès");
+                    }
+                }
+            }
+                
+        }
+    }
+}
+
